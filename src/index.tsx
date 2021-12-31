178d38e26515bb7babaeaf7951cdf8dec2bab1f2
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2021-12-31 20:25:44 +0530

    refactor file structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,14 @@
-import { useState } from "react";
 import ReactDOM from "react-dom";
 
-import CodeEditor from "./components/code-editor";
-import Preview from "./components/preview";
-
-import bundle from "./bundler";
+import CodeCell from "./components/code-cell";
 
 // CSS
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 const App = () => {
-  const [code, setCode] = useState("");
-  const [input, setInput] = useState("");
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="// Code here"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
