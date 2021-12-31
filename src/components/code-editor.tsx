e4f117d8ebae103c8885596f96e9c85334c3aab7
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2021-12-31 19:05:33 +0530

    configure monaco-editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,35 @@
 import MonacoEditor from "@monaco-editor/react";
+// import * as monacoEditor from "monaco-editor";
 
 const CodeEditor = () => {
-  return <MonacoEditor height="500px" language="javascript" theme="dark" />;
+  //   const editorOptions: monacoEditor.editor.IEditorConstructionOptions = {
+  //     wordWrap: "on",
+  //     minimap: { enabled: false },
+  //     showUnused: false,
+  //     folding: false,
+  //     lineNumbersMinChars: 3,
+  //     fontSize: 16,
+  //     scrollBeyondLastLine: true,
+  //     automaticLayout: true,
+  //   };
+
+  return (
+    <MonacoEditor
+      height="500px"
+      language="javascript"
+      theme="dark"
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: true,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
