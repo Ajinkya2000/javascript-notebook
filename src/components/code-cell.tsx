4af64ed64953d3289fc9f68e1ba8e3bef4cd6206
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2022-03-20 12:09:05 +0530

    add logic to show cumulative code execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,73 +1,89 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 // import bundle from "../bundler";
-import { Cell } from "../state";
-import { useActions } from "../hooks/use-actions";
+import { Cell } from '../state';
+import { useActions } from '../hooks/use-actions';
 
-import CodeEditor from "./code-editor";
-import Preview from "./preview";
-import Resizable from "./resizable";
-import { useTypedSelector } from "../hooks/use-typed-selector";
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import Resizable from './resizable';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 
 // CSS
-import "./code-cell.css";
+import './code-cell.css';
 
 interface CodeCellProps {
-  cell: Cell;
+	cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-  const { updateCell, createBundle } = useActions();
-  const bundle = useTypedSelector((state) => {
-    return state.bundles[cell.id];
-  });
+	const { updateCell, createBundle } = useActions();
+	const bundle = useTypedSelector((state) => {
+		return state.bundles[cell.id];
+	});
+	const cumulativeCode = useTypedSelector((state) => {
+		const { data, order } = state.cells;
+		const orderedCells = order.map((id) => data[id]);
 
-  useEffect(() => {
-    if (!bundle) {
-      createBundle(cell.id, cell.content);
-      return;
-    }
+		const cumulativeCode = [];
+		for (let c of orderedCells) {
+			if (c.type === 'code') {
+				cumulativeCode.push(c.content);
+			}
 
-    const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
-    }, 750);
+			if (c.id === cell.id) {
+				break;
+			}
+		}
+		return cumulativeCode;
+	});
 
-    return () => {
-      clearTimeout(timer);
-    };
+	useEffect(() => {
+		if (!bundle) {
+			createBundle(cell.id, cumulativeCode.join('\n'));
+			return;
+		}
 
-    //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
+		const timer = setTimeout(async () => {
+			createBundle(cell.id, cumulativeCode.join('\n'));
+		}, 750);
 
-  return (
-    <Resizable direction="vertical">
-      <div
-        style={{
-          height: "calc(100% - 10px)",
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <Resizable direction="horizontal">
-          <CodeEditor
-            initialValue={cell.content}
-            onChange={(value) => updateCell(cell.id, value)}
-          />
-        </Resizable>
-        <div className="progress-wrapper">
-          {!bundle || bundle.loading ? (
-            <div className="progress-cover">
-              <progress className="progress is-small is-primary" max={100}>
-                Loading
-              </progress>
-            </div>
-          ) : (
-            <Preview code={bundle.code} err={bundle.err} />
-          )}
-        </div>
-      </div>
-    </Resizable>
-  );
+		return () => {
+			clearTimeout(timer);
+		};
+
+		//eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [cumulativeCode.join('\n'), cell.id, createBundle]);
+
+	return (
+		<Resizable direction='vertical'>
+			<div
+				style={{
+					height: 'calc(100% - 10px)',
+					display: 'flex',
+					flexDirection: 'row',
+				}}
+			>
+				<Resizable direction='horizontal'>
+					<CodeEditor
+						initialValue={cell.content}
+						onChange={(value) => updateCell(cell.id, value)}
+					/>
+				</Resizable>
+				<div className='progress-wrapper'>
+					{!bundle || bundle.loading ? (
+						<div className='progress-cover'>
+							<progress className='progress is-small is-primary' max={100}>
+								Loading
+							</progress>
+						</div>
+					) : (
+						<Preview code={bundle.code} err={bundle.err} />
+					)}
+				</div>
+			</div>
+		</Resizable>
+	);
 };
 
 export default CodeCell;
